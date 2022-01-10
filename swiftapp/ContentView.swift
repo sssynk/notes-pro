e55835a549e923fde53520cc02af769908ef9193
James Wilson <james@jamesssystems.info>
2022-01-10 11:26:21 -0800

    Initial Commit

---
+++ swiftapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftapp
+//
+//  Created by James Wilson on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftapp/swiftappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftappApp.swift
+//  swiftapp
+//
+//  Created by James Wilson on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
