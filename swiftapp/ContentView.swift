51c14b74d21fe446c52c7409154fdadc75e2b49b
James Wilson <james@jamesssystems.info>
2022-01-10 12:44:26 -0800

    Amazing Commit! Added 5 new errors! woo

--- swiftapp/ContentView.swift
+++ swiftapp/ContentView.swift
@@ -6,11 +6,70 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @AppStorage("name") var name = "User"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Text("james.baby notes")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("sign in")
+                .padding(.top, 60)
+                .font(.title)
+            ZStack {
+                TextField("Username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: 400, height: 50)
+                    .padding(.top, 350)
+                    .textFieldStyle(.automatic)
+                    .font(Font.system(size: 20, design: .default))
+                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: 400, height: 50)
+                    .padding(.top, 420)
+                    .textFieldStyle(.automatic)
+                    .font(Font.system(size: 20, design: .rounded))
+                
+                Button("Log In") {
+                    logUser()
+                }
+                .padding(.top, 500)
+            }
+            
+        }
+        .padding(.leading, 50.0)
+        .padding(.trailing, 50.0)
+        .padding(.bottom, 300.0)
+    }
+    func logUser() {
+        let request = requestLogin(username: "synk", password: "synk")
+        
+    }
+    func requestLogin(username: String, password: String) {
+        let req = query(address: URL(string: "https://api.james.baby/s/internal/login?username=\(username)&password=\(password)")!)
+        if(req.statusCode == 200) {
+            print("WELCOME!!")
+        } else {
+            print("BRUH")
+        }
+    }
+    func query(address: URL) -> HTTPURLResponse {
+        var url = URLRequest(url: address)
+        url.setValue("https://james.baby", forHTTPHeaderField: "origin")
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var result: HTTPURLResponse
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            result = (response as? HTTPURLResponse)!
+            semaphore.signal()
+        }
+        
+        task.resume()
+        semaphore.wait()
+        return result
     }
 }
 
--- swiftapp/swiftappApp.swift
+++ swiftapp/swiftappApp.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 @main
 struct swiftappApp: App {
+    init() {
+        UserDefaults.standard.register(defaults: [
+            "name": "James Wilson",
+            "highScore": 10
+        ])
+        
+
+        if let url = URL(string: "https://api.james.baby/school/getcurrentalerts") {
+            do {
+                let contents = try String(contentsOf: url)
+                print(contents)
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // the URL was bad!
+        }
+
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
